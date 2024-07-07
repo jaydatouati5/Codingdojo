0359b7b80a6b6d9a010e73bf10072e6867bb5e4d
jaydatouati5 <jaydatouati5@gmail.com>
2024-07-07 16:02:39 +0100

    Ninja

--- MERN Stack/JavaScript v 23.0/Practice/Ninja/script.js
+++ MERN Stack/JavaScript v 23.0/Practice/Ninja/script.js
@@ -1,9 +1,9 @@
 class Ninja{
-    constructor(name){
+    constructor(name, health=90,speed=3,strength=3){
         this.name = name;
-        this.health = 90;
-        this.speed =3;
-        this.strength =3;
+        this.health = health;
+        this.speed =speed;
+        this.strength =strength;
     }
     sayName(){
         console.log(`Hello ${this.name}`);
