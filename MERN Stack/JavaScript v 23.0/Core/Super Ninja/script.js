29c57c1486f87949e5815d2fe6a7f4a8b44b661f
jaydatouati5 <jaydatouati5@gmail.com>
2024-07-07 15:58:20 +0100

    Super Ninja

---
+++ MERN Stack/JavaScript v 23.0/Core/Super Ninja/script.js
@@ -0,0 +1,28 @@
+class Ninja{
+    constructor(name,health=90,speed=3,strength=3){
+        this.name = name;
+        this.health = health;
+        this.speed =speed;
+        this.strength =strength;
+    }
+    sayName(){
+        console.log(`Hello ${this.name}`);
+    }
+    showStats(){
+        console.log(`${this.name} ${this.health} ${this.speed} ${this.strength}`);
+    }
+    drinkSake(){
+        this.health += 10;
+    }
+}
+
+class Sensei extends Ninja{
+    constructor(name,health=200,speed=10,strength=10,wisdom=10){
+        super(name,health,speed,strength);
+        this.wisdom = wisdom ;
+    }
+    speakWisdom(){
+        super.drinkSake()
+        console.log("No pain, no gain")
+    }
+}
