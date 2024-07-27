ee5960045ddf63534b6ab04a7b87a748d3d339c1
jaydatouati5 <jaydatouati5@gmail.com>
2024-07-27 19:21:33 +0100

    Todo List

--- MERN Stack/React v 23.0/Core/ToDo List/todo-list/src/components/Task.jsx
+++ MERN Stack/React v 23.0/Core/ToDo List/todo-list/src/components/Task.jsx
@@ -5,8 +5,8 @@ const Task = ({content , done , index , triggerTask , deleteTask}) => {
     return (
         <div  className={styles.task}>
             <span>
-                <input type="checkbox" name="" id="fstring" onChange={() => triggerTask(index)}/>
-                <label htmlFor="fstring">
+                <input type="checkbox" name="" id={`${index}_task`} onChange={() => triggerTask(index)}/>
+                <label htmlFor={`${index}_task`}>
                     <span style={{textDecoration: done ? 'line-through' : 'none'}}>{content}</span>
                 </label>
             </span>
