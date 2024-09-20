0b10899fc2f534da819a0e72524160ef4dba1286
jaydatouati5 <jaydatouati5@gmail.com>
2024-09-20 19:03:24 +0100

    First Java Program

---
+++ Java/Java Fundemantals/Practice/First Java Program/Greetings.java
@@ -0,0 +1,8 @@
+public class Greetings {
+    public static void main(String[]args){
+        // System.out.println("My name is Jouda");
+        // System.out.println("I am 21 years old");
+        // System.out.println("I'm from Gabes");
+        System.out.println("My name is Jouda\nI am 21 years old\nI'm from Gabes");
+    }
+}
