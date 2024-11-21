2a2c72b308d2bf0abb895f08954c4625c5a8c6fb
jaydatouati5 <jaydatouati5@gmail.com>
2024-11-21 10:27:10 +0100

    Ride Mader

---
+++ C# .net/Language Fundamentals/Practice/RideMader/Program.cs
@@ -0,0 +1,15 @@
+﻿Vehicle car = new ("lamborghini", "red");
+Vehicle horse = new ("Monica", "brown");
+Vehicle unicycle = new ("catchy", "black");
+Vehicle yacht = new ("Monica", "brown");
+
+List<Vehicle> VehicleList = new() {car, horse, unicycle, yacht};
+
+foreach(Vehicle vehicle in VehicleList)
+{
+    vehicle.ShowInfo();
+}
+
+unicycle.Travel(100);
+unicycle.ShowInfo();
+
---
+++ C# .net/Language Fundamentals/Practice/RideMader/Vehicle.cs
@@ -0,0 +1,39 @@
+class Vehicle 
+{
+    string Name;
+    int Number;
+    string Color;
+    bool HasEngine;
+    int Mileage = 0;
+
+    public Vehicle(string name, int number, string color, bool hasEngine )
+    {
+        Name = name;
+        Number = number;
+        Color = color;
+        HasEngine = hasEngine;
+    }
+
+    public Vehicle (string name,string color)
+    {
+        Name = name;
+        Color = color;
+        Number= 0;
+        HasEngine = true;
+    }
+
+    public void ShowInfo()
+    {
+        Console.WriteLine(Name);
+        Console.WriteLine(Number);
+        Console.WriteLine(Color);
+        Console.WriteLine(HasEngine);
+        Console.WriteLine(Mileage);
+    }
+
+    public void Travel(int distance) 
+    {
+        Mileage+=distance;
+        Console.WriteLine($"The Vehicle has travelled {Mileage} miles");
+    }
+}
