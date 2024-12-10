d160bb31af17e5c34db3edb6ea09e6177bd3a545
jaydatouati5 <jaydatouati5@gmail.com>
2024-12-10 13:44:52 +0100

    Dojo Survey With Model

---
+++ C# .net/ASP .net core/Practice/DojoSurveywithModel/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using DojoSurveywithModel.Models;
+
+namespace DojoSurveywithModel.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+
+    [HttpGet("")]
+    public IActionResult Index()
+    {
+
+        return View();
+    }
+
+
+    [HttpPost("results")]
+    public IActionResult Results(Survey survey)
+    {
+        return View(survey);
+    }
+
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ C# .net/ASP .net core/Practice/DojoSurveywithModel/Models/Survey.cs
@@ -0,0 +1,12 @@
+#pragma warning disable CS8618
+
+namespace DojoSurveywithModel;
+
+public class Survey{
+
+    public string Name {get;set;}
+    public string DojoLocation {get;set;}
+    public string FavoriteLanguage {get;set;}
+    public string Comment {get;set;}
+
+}
