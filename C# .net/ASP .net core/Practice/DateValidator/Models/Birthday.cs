96fb61d8f667160a59bc9ec5a126257fb7efc94d
jaydatouati5 <jaydatouati5@gmail.com>
2024-12-13 17:16:41 +0100

    Date Validator

---
+++ C# .net/ASP .net core/Practice/DateValidator/Models/Birthday.cs
@@ -0,0 +1,32 @@
+#pragma warning disable CS8618
+
+namespace DateValidator;
+
+using System.ComponentModel.DataAnnotations;
+
+public class Birthday 
+{
+    [FutureDate]
+    public DateTime Date {get; set;}
+
+}
+
+public class FutureDateAttribute : ValidationAttribute
+{    
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)    
+    {        
+        // You first may want to unbox "value" here and cast to to a DateTime variable!  
+        DateTime date = (DateTime)value;
+        DateTime now = DateTime.Now;
+
+        if (date > now)
+        {
+            return new ValidationResult("Error, birthday's can't be in the future.");
+        }
+        else
+        {
+            return ValidationResult.Success;
+        }
+    }
+}
+
